2718653181914ba21e361d67d04268b921608fbc
Kauan Santos <kauansantos1411@outlook.com>
2025-10-21 19:32:43 -0300

    style(CursoViewer): Responsividade do CursoHeader

--- src/components/users/cursoViewer/components/CursoHeader.tsx
+++ src/components/users/cursoViewer/components/CursoHeader.tsx
@@ -33,35 +33,36 @@ export function CursoHeader({ curso, isEnrolled, isEnrolling, onEnroll }: CursoH
   const estiloAprendizagemInfo = estiloInfo[curso.estiloAprendizagem.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLocaleLowerCase() as keyof typeof estiloInfo];
 
   return (
-    <div className="flex items-center justify-between">
+    <div className="flex flex-col gap-5 sm:flex-row items-center justify-between sm:gap-0">
       <div className="flex items-center space-x-3">
-        <div>
-          <Link href={voltaBotaoHref()}>
-            <Button variant="ghost" size="sm">
-              <ArrowLeft className="h-4 w-4 mr-2" />
-              Voltar
-            </Button>
-          </Link>
-        </div>
-        <div className="space-y-2">
-          <h1 className="text-3xl font-bold text-gray-900 dark:text-white">{curso.titulo}</h1>
-          <div className="flex items-center space-x-4 text-sm text-gray-600 dark:text-gray-400">
-            <div className="flex items-center">
-              <Star className="h-4 w-4 mr-1 fill-yellow-400 text-yellow-400" />
-              <span>{curso.avaliacao}</span>
+        <div className="flex items-center">
+          <div>
+            <Link href={voltaBotaoHref()}>
+              <Button variant="ghost" size="icon">
+                <ArrowLeft className="h-4 w-4" />
+              </Button>
+            </Link>
+          </div>
+          <div className="space-y-2">
+            <h1 className="text-3xl font-bold text-gray-900 dark:text-white">{curso.titulo}</h1>
+            <div className="flex items-center space-x-4 text-sm text-gray-600 dark:text-gray-400">
+              <div className="flex items-center">
+                <Star className="h-4 w-4 mr-1 fill-yellow-400 text-yellow-400" />
+                <span>{curso.avaliacao}</span>
+              </div>
+              <div className="flex items-center">
+                <Users className="h-4 w-4 mr-1" />
+                <span>{curso.alunos.toLocaleString()} estudantes</span>
+              </div>
+              <div className="flex items-center">
+                <Clock className="h-4 w-4 mr-1" />
+                <span>{curso.duracaoTotal}</span>
+              </div>
+              <Badge variant="secondary">{curso.nivel}</Badge>
+              <Badge variant="secondary" className={`${estiloAprendizagemInfo.bgColor} ${estiloAprendizagemInfo.textColor} border ${estiloAprendizagemInfo.borderColor} no-hover dark:bg-transparent`}>
+                {curso.estiloAprendizagem}
+              </Badge>
             </div>
-            <div className="flex items-center">
-              <Users className="h-4 w-4 mr-1" />
-              <span>{curso.alunos.toLocaleString()} estudantes</span>
-            </div>
-            <div className="flex items-center">
-              <Clock className="h-4 w-4 mr-1" />
-              <span>{curso.duracaoTotal}</span>
-            </div>
-            <Badge variant="secondary">{curso.nivel}</Badge>
-            <Badge variant="secondary" className={`${estiloAprendizagemInfo.bgColor} ${estiloAprendizagemInfo.textColor} border ${estiloAprendizagemInfo.borderColor} no-hover dark:bg-transparent`}>
-              {curso.estiloAprendizagem}
-            </Badge>
           </div>
         </div>
       </div>
